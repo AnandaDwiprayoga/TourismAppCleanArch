b17218984e7d02f5403cb7792532f747e566253a
paslang <anandadwiprayoga@192.168.1.16>
2021-07-03 00:11:20 +0700

    change livedata to flow in domain and data layer, presentation layer still using livedata cause is actual implementation

--- app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
@@ -1,14 +1,14 @@
 package com.dicoding.tourismapp.core.data.source.remote
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiResponse
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiService
-import com.dicoding.tourismapp.core.data.source.remote.response.ListTourismResponse
 import com.dicoding.tourismapp.core.data.source.remote.response.TourismResponse
-import retrofit2.Call
-import retrofit2.Response
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import java.lang.Exception
 
 class RemoteDataSource private constructor(private val apiService: ApiService) {
     companion object {
@@ -21,28 +21,42 @@ class RemoteDataSource private constructor(private val apiService: ApiService) {
             }
     }
 
-    fun getAllTourism(): LiveData<ApiResponse<List<TourismResponse>>> {
-        val resultData = MutableLiveData<ApiResponse<List<TourismResponse>>>()
-
-        //get data from local json
-        val client = apiService.getList()
-
-        client.enqueue(object : retrofit2.Callback<ListTourismResponse> {
-            override fun onResponse(
-                call: Call<ListTourismResponse>,
-                response: Response<ListTourismResponse>
-            ) {
-                val dataArray = response.body()?.places
-                resultData.value = if(dataArray != null)ApiResponse.Success(dataArray) else ApiResponse.Empty
+    fun getAllTourism() : Flow<ApiResponse<List<TourismResponse>>> =
+        flow{
+            try {
+                val response = apiService.getList()
+                val dataArray = response.places
+                if(dataArray.isNotEmpty()){
+                    emit(ApiResponse.Success(dataArray))
+                }else{
+                    emit(ApiResponse.Empty)
+                }
+            }catch (e: Exception){
+                emit(ApiResponse.Error(e.toString()))
             }
-
-            override fun onFailure(call: Call<ListTourismResponse>, t: Throwable) {
-                resultData.value = ApiResponse.Error(t.message.toString())
-                Log.e("Remote Data Source", t.message.toString())
-            }
-
-        })
-        return resultData
-    }
+        }.flowOn(Dispatchers.IO)
+//        {
+//        val resultData = MutableLiveData<ApiResponse<List<TourismResponse>>>()
+//
+//        //get data from local json
+//        val client = apiService.getList()
+//
+//        client.enqueue(object : retrofit2.Callback<ListTourismResponse> {
+//            override fun onResponse(
+//                call: Call<ListTourismResponse>,
+//                response: Response<ListTourismResponse>
+//            ) {
+//                val dataArray = response.body()?.places
+//                resultData.value = if(dataArray != null)ApiResponse.Success(dataArray) else ApiResponse.Empty
+//            }
+//
+//            override fun onFailure(call: Call<ListTourismResponse>, t: Throwable) {
+//                resultData.value = ApiResponse.Error(t.message.toString())
+//                Log.e("Remote Data Source", t.message.toString())
+//            }
+//
+//        })
+//        return resultData
+//    }
 }
 
