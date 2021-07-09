01659957f9a0e337040ac4639875e03323bc57b1
paslang <anandadwiprayoga@192.168.1.13>
2021-07-09 21:09:49 +0700

    encrypt database with sqllitecipher

--- core/src/main/java/id/pasukanlangit/core/di/DatabaseModule.kt
+++ core/src/main/java/id/pasukanlangit/core/di/DatabaseModule.kt
@@ -2,6 +2,7 @@ package id.pasukanlangit.core.di
 
 import android.content.Context
 import androidx.room.Room
+import net.sqlcipher.database.SQLiteDatabase
 import id.pasukanlangit.core.data.source.local.room.TourismDao
 import id.pasukanlangit.core.data.source.local.room.TourismDatabase
 import dagger.Module
@@ -9,17 +10,24 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
 
+    private val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+    val factory = SupportFactory(passphrase)
+
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context) : TourismDatabase =
-        Room.databaseBuilder(context, TourismDatabase::class.java, "Tourism.db").fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): TourismDatabase =
+        Room.databaseBuilder(context, TourismDatabase::class.java, "Tourism.db")
+            .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
 
     @Provides
-    fun provideTourismDabo(database: TourismDatabase) : TourismDao = database.tourismDao()
+    fun provideTourismDabo(database: TourismDatabase): TourismDao = database.tourismDao()
 }
